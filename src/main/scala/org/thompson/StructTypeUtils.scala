41c2b0eadb39e6c6a99ce84c509384f607092811
ajaythompson <ajay.thompson@thedatateam.in>
2019-09-15 20:38:36 +0530

    spark session to be passed as implicit instead of spark context

--- src/main/scala/org/thompson/StructTypeUtils.scala
+++ src/main/scala/org/thompson/StructTypeUtils.scala
@@ -8,13 +8,17 @@ import org.scalacheck.Gen
 
 object StructTypeUtils {
   implicit class dataGenerator(structType: StructType) {
+    /**
+     * Fetch a random dataframe for the give row generator
+     * @param rowGen Generator that yields a row
+     * @param numSlice number of slice  or splits
+     * @param numRowPerSlice number of records per slice
+     * @param spark spark session
+     * @return random row dataframe
+     */
       def dataGenerator(rowGen: Gen[Row], numSlice: Int, numRowPerSlice: Int)
-                       (implicit sc: SparkContext): DataFrame = {
-        val spark: SparkSession = SparkSession
-          .builder()
-          .config(sc.getConf)
-          .getOrCreate()
-
+                       (implicit spark: SparkSession): DataFrame = {
+        val sc = spark.sparkContext
         lazy val rowSeq: Seq[Row] = Gen.listOfN(numRowPerSlice, rowGen).sample.get
         val genRDD: RDD[Seq[Row]] = sc.parallelize(List.fill(numSlice)(rowSeq), numSlice)
         val rowRdd: RDD[Row] = genRDD.flatMap(x => x)
