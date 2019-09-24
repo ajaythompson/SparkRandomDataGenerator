65b3c7d0ba9aa513663c21f3e4cd3958815c911b
ajaythompson <ajay.thompson@thedatateam.in>
2019-09-24 11:29:19 +0530

    Generator produces random value during split creation

--- src/main/scala/org/thompson/StructTypeUtils.scala
+++ src/main/scala/org/thompson/StructTypeUtils.scala
@@ -1,28 +1,28 @@
 package org.thompson
 
-import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.scalacheck.Gen
 
 object StructTypeUtils {
   implicit class dataGenerator(structType: StructType) {
     /**
      * Fetch a random dataframe for the give row generator
-     * @param rowGen Generator that yields a row
-     * @param numSlice number of slice  or splits
+     *
+     * @param rowGen         Generator that yields a row
+     * @param numSlice       number of slice  or splits
      * @param numRowPerSlice number of records per slice
-     * @param spark spark session
+     * @param spark          spark session
      * @return random row dataframe
      */
-      def dataGenerator(rowGen: Gen[Row], numSlice: Int, numRowPerSlice: Int)
-                       (implicit spark: SparkSession): DataFrame = {
-        val sc = spark.sparkContext
-        lazy val rowSeq: Seq[Row] = Gen.listOfN(numRowPerSlice, rowGen).sample.get
-        val genRDD: RDD[Seq[Row]] = sc.parallelize(List.fill(numSlice)(rowSeq), numSlice)
-        val rowRdd: RDD[Row] = genRDD.flatMap(x => x)
-        spark.createDataFrame(rowRdd, structType)
-      }
+    def dataGenerator(rowGen: Gen[Row], numSlice: Int, numRowPerSlice: Int)
+                     (implicit spark: SparkSession): DataFrame = {
+      val sc = spark.sparkContext
+      lazy val rowSeq: Gen[List[Row]] = Gen.listOfN(numRowPerSlice, rowGen)
+      val genRDD: RDD[Seq[Row]] = sc.parallelize(List.fill(numSlice)(rowSeq.sample.get), numSlice)
+      val rowRdd: RDD[Row] = genRDD.flatMap(x => x)
+      spark.createDataFrame(rowRdd, structType)
+    }
   }
 }
